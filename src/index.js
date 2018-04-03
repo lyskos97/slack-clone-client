8b2f2530a601d17d0aba9eea38b4ffe251c4482d
Konstantin Lyssenko <lyskos97@gmail.com>
2018-04-04 02:53:17 +0600

    feat: set auth headers for apollo requests

--- src/index.js
+++ src/index.js
@@ -6,7 +6,23 @@ import { ApolloProvider } from 'react-apollo';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
-const client = new ApolloClient({ uri: 'http://localhost:4000' });
+/* 
+  TODO: change boost Client to native apollo-client to enable afterwares for obsolete tokens replacement
+*/
+const token = localStorage.getItem('token');
+const refreshToken = localStorage.getItem('refreshToken');
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  fetchOptions: {
+    headers: {
+      authorization: {
+        'x-token': token,
+        'x-refresh-token': refreshToken,
+      },
+    },
+  },
+});
 
 const WrappedApp = () => (
   <ApolloProvider client={client}>
